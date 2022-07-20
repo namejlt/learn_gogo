816b217b4ab37edff7d054e211c7075e0e73d4e4
alexweishao <1633014815@qq.com>
2022-07-20 22:28:58 +0800

    使用gorm连接数据库mysql

--- working/mini_blog/main.go
+++ working/mini_blog/main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 	"mini_blog/routers"
 )
 
 func main() {
+	//用户名：密码@tcp（ip:port）/数据库？charset=utf8&parseTime=true&loc=Local
+	db, err := gorm.Open("mysql", "root:kk123456yy@tcp(localhost:3306)/weibo?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close() //关闭空闲连接，防止内存泄漏
+
 	//默认路由引擎
 	router := gin.Default()
 
