578776bce207601d667218acbcd3c6afee735544
alexweishao <1633014815@qq.com>
2022-07-21 00:15:36 +0800

    创建数据库表的三种方式

---
+++ working/mini_blog/models/message.go
@@ -0,0 +1,7 @@
+package models
+
+// Message 用户发送消息表
+type Message struct {
+	Title   string
+	Content string
+}
---
+++ working/mini_blog/models/user_login.go
@@ -0,0 +1,7 @@
+package models
+
+// UserLoginMessage 用户登录信息表
+type UserLoginMessage struct {
+	UserName string
+	Password string
+}
